4cf98fdcf78c031e264e4de83b1428833519c705
Syed Shahir Ahmed Rakin <38401561+syedrakin707@users.noreply.github.com>
2019-12-24 20:51:11 +0600

    Finalized Version

--- Syed Shahir Ahmed Rakin/stock_prediction_bank.py
+++ Syed Shahir Ahmed Rakin/stock_prediction_bank.py
@@ -17,9 +17,9 @@ from pyESN import ESN
 #from matplotlib.pylab import rcParams
 #rcParams['figure.figsize'] = 20,10
 
-n_reservoir=500
-sparsity=0.4
-rand_seed=15
+n_reservoir= 600
+sparsity= 0.4
+rand_seed= 20
 spectral_radius = 1.2
 noise = .0010
 
@@ -67,9 +67,9 @@ print(dfabbank_N)
 #for i in range(0,len(df)):
 #     dfabbank['CLOSEP (ABBANK)'][i] = df['CLOSEP (ABBANK)'][i]
 
-train_len = 1000
+train_len = 1100
 future = 10
-futureTotal=50
+futureTotal= 30
 pred_tot_abbank=np.zeros(futureTotal)
 pred_tot_bankasia=np.zeros(futureTotal)
 pred_tot_ebl=np.zeros(futureTotal)
@@ -130,21 +130,22 @@ for i in range(0,futureTotal,future):
     pred_training = esn.fit(np.ones(train_len),dfucb_N[i:train_len+i])
     prediction = esn.predict(np.ones(future))
     pred_tot_ucb[i:i+future] = prediction[:,0]
+  
     
-
-fig,axs1 = plt.subplots(2,1)
+#Graph Section Starts Here
+    
+fig,axs1 = plt.subplots(2)
 fig.subplots_adjust(hspace=.5)
 
-axs1[0].plot(range(00,train_len+futureTotal),dfabbank[00:train_len+futureTotal],'b',label="Original Data (AB Bank)", alpha=0.3)
+axs1[0].plot(range(00,train_len+futureTotal),dfabbank_N[00:train_len+futureTotal],'b',label="Original Data (AB Bank)", alpha=0.3)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs1[0].plot(range(train_len,train_len+futureTotal),pred_tot_abbank,'k',  alpha=0.8, label='Post ESN Data (AB Bank)')
 
-axs1[1].plot(range(00,train_len+futureTotal),dfbankasia[00:train_len+futureTotal],'y',label="Original Data (Bank Asia)", alpha=0.3)
+axs1[1].plot(range(00,train_len+futureTotal),dfbankasia_N[00:train_len+futureTotal],'y',label="Original Data (Bank Asia)", alpha=0.3)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs1[1].plot(range(train_len,train_len+futureTotal),pred_tot_bankasia,'r',  alpha=0.8, label='Post ESN Data (Bank Asia)')
-lo,hi = plt.ylim()
-axs1[0].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'b--', linewidth=4)
-axs1[1].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'r--', linewidth=4)
+axs1[0].plot([train_len,train_len],[dfabbank_N.min(),dfabbank_N.max()],'b--', linewidth=2)
+axs1[1].plot([train_len,train_len],[dfbankasia_N.min(),dfbankasia_N.max()],'r--', linewidth=2)
 
 axs1[0].set_title('Ground Truth and Echo State Network Output', fontsize=12)
 axs1[1].set_title('Ground Truth and Echo State Network Output', fontsize=12)
@@ -157,19 +158,18 @@ axs1[1].legend(bbox_to_anchor=(0.80, 1.2), fontsize='small', loc=2, borderaxespa
 plt.show()
 ###################################
 
-fig,axs2 = plt.subplots(2,1)
+fig,axs2 = plt.subplots(2)
 fig.subplots_adjust(hspace=.5)
 
-axs2[0].plot(range(00,train_len+futureTotal),dfific[00:train_len+futureTotal],'b',label="Original Data (IFIC)", alpha=0.4)
+axs2[0].plot(range(00,train_len+futureTotal),dfific_N[00:train_len+futureTotal],'b',label="Original Data (IFIC)", alpha=0.4)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[0].plot(range(train_len,train_len+futureTotal),pred_tot_ific,'k',  alpha=0.8, label='Post ESN Data (IFIC)')
 
-axs2[1].plot(range(00,train_len+futureTotal),dfjamunabank[00:train_len+futureTotal],'y',label="Original Data (Jamuna Bank)", alpha=0.7)
+axs2[1].plot(range(00,train_len+futureTotal),dfjamunabank_N[00:train_len+futureTotal],'y',label="Original Data (Jamuna Bank)", alpha=0.7)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[1].plot(range(train_len,train_len+futureTotal),pred_tot_jamunabank,'r',  alpha=0.8, label='Post ESN Data (Jamuna Bank)')
-lo,hi = plt.ylim()
-axs2[0].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'b--', linewidth=4)
-axs2[1].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'r--', linewidth=4)
+axs2[0].plot([train_len,train_len],[dfific_N.min(),dfific_N.max()],'b--', linewidth=2)
+axs2[1].plot([train_len,train_len],[dfjamunabank_N.min(),dfjamunabank_N.max()],'r--', linewidth=2)
 
 axs2[0].set_title('Ground Truth and Echo State Network Output', fontsize=12)
 axs2[1].set_title('Ground Truth and Echo State Network Output', fontsize=12)
@@ -183,19 +183,18 @@ plt.show()
 
 ##################################
 
-fig,axs2 = plt.subplots(2,1)
+fig,axs2 = plt.subplots(2)
 fig.subplots_adjust(hspace=.5)
 
-axs2[0].plot(range(00,train_len+futureTotal),dfmtb[00:train_len+futureTotal],'b',label="Original Data (MTB)", alpha=0.4)
+axs2[0].plot(range(00,train_len+futureTotal),dfmtb_N[00:train_len+futureTotal],'b',label="Original Data (MTB)", alpha=0.4)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[0].plot(range(train_len,train_len+futureTotal),pred_tot_mtb,'k',  alpha=0.8, label='Post ESN Data (MTB)')
 
-axs2[1].plot(range(00,train_len+futureTotal),dfnccbank[00:train_len+futureTotal],'y',label="Original Data (NCC Bank)", alpha=0.7)
+axs2[1].plot(range(00,train_len+futureTotal),dfnccbank_N[00:train_len+futureTotal],'y',label="Original Data (NCC Bank)", alpha=0.7)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[1].plot(range(train_len,train_len+futureTotal),pred_tot_nccbank,'r',  alpha=0.8, label='Post ESN Data (NCC Bank)')
-lo,hi = plt.ylim()
-axs2[0].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'b--', linewidth=4)
-axs2[1].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'r--', linewidth=4)
+axs2[0].plot([train_len,train_len],[dfmtb_N.min(),dfmtb_N.max()],'b--', linewidth=2)
+axs2[1].plot([train_len,train_len],[dfnccbank_N.min(),dfnccbank_N.max()],'r--', linewidth=2)
 
 axs2[0].set_title('Ground Truth and Echo State Network Output', fontsize=12)
 axs2[1].set_title('Ground Truth and Echo State Network Output', fontsize=12)
@@ -209,22 +208,22 @@ plt.show()
 
 ##################################
 
-fig,axs2 = plt.subplots(2,1)
+fig,axs2 = plt.subplots(2)
 fig.subplots_adjust(hspace=.5)
 
-axs2[0].plot(range(00,train_len+futureTotal),dfsoutheast[00:train_len+futureTotal],'b',label="Original Data (South East Bank)", alpha=0.4)
+axs2[0].plot(range(00,train_len+futureTotal),dfsoutheast_N[00:train_len+futureTotal],'b',label="Original Data (South East Bank)", alpha=0.4)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[0].plot(range(train_len,train_len+futureTotal),pred_tot_southeast,'k',  alpha=0.8, label='Post ESN Data (South East Bank)')
 
-axs2[1].plot(range(00,train_len+futureTotal),dfucb[00:train_len+futureTotal],'y',label="Original Data (UCB)", alpha=0.7)
+axs2[1].plot(range(00,train_len+futureTotal),dfucb_N[00:train_len+futureTotal],'y',label="Original Data (UCB)", alpha=0.7)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[1].plot(range(train_len,train_len+futureTotal),pred_tot_ucb,'r',  alpha=0.8, label='Post ESN Data (UCB)')
 lo,hi = plt.ylim()
-axs2[0].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'b--', linewidth=4)
-axs2[1].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'r--', linewidth=4)
+axs2[0].plot([train_len,train_len],[dfsoutheast_N.min(),dfsoutheast_N.max()],'b--', linewidth=2)
+axs2[1].plot([train_len,train_len],[dfucb_N.min(),dfucb_N.max()],'r--', linewidth=2)
 
-axs2[0].set_title('Ground Truth and Echo State Network Output', fontsize=12)
-axs2[1].set_title('Ground Truth and Echo State Network Output', fontsize=12)
+axs2[0].set_title('Share Status of South East Bank (Used ESN)', fontsize=12)
+axs2[1].set_title('Share Status of UCB (Used ESN)', fontsize=12)
 
 for ax in axs2.flat:
     ax.set(xlabel='Time (Days)', ylabel='Price ($)')
@@ -235,22 +234,21 @@ plt.show()
 
 ##################################
 
-fig,axs2 = plt.subplots(2,1)
+fig,axs2 = plt.subplots(2)
 fig.subplots_adjust(hspace=.5)
 
-axs2[0].plot(range(00,train_len+futureTotal),dfebl[00:train_len+futureTotal],'b',label="Original Data (EBL)", alpha=0.4)
+axs2[0].plot(range(00,train_len+futureTotal),dfebl_N[00:train_len+futureTotal],'b',label="Original Data (EBL)", alpha=0.4)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[0].plot(range(train_len,train_len+futureTotal),pred_tot_ebl,'k',  alpha=0.8, label='Post ESN Data (EBL)')
 
-axs2[1].plot(range(00,train_len+futureTotal),dfdutchbangl[00:train_len+futureTotal],'y',label="Original Data (DBBL)", alpha=0.7)
+axs2[1].plot(range(00,train_len+futureTotal),dfdutchbangl_N[00:train_len+futureTotal],'y',label="Original Data (DBBL)", alpha=0.7)
 #plt.plot(range(0,train_len),pred_training,'.g',  alpha=0.3)
 axs2[1].plot(range(train_len,train_len+futureTotal),pred_tot_dutchbangl,'r',  alpha=0.8, label='Post ESN Data (DBBL)')
-lo,hi = plt.ylim()
-axs2[0].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'b--', linewidth=4)
-axs2[1].plot([train_len,train_len],[lo+np.spacing(1),hi-np.spacing(1)],'r--', linewidth=4)
+axs2[0].plot([train_len,train_len],[dfebl_N.min(),dfebl_N.max()],'b--', linewidth=2)
+axs2[1].plot([train_len,train_len],[dfdutchbangl_N.min(),dfdutchbangl_N.max()],'r--', linewidth=2)
 
-axs2[0].set_title('Ground Truth and Echo State Network Output', fontsize=12)
-axs2[1].set_title('Ground Truth and Echo State Network Output', fontsize=12)
+axs2[0].set_title('Share Status of EBL (Used ESN)', fontsize=12)
+axs2[1].set_title('Share Status of DBBL (Used ESN)', fontsize=12)
 
 for ax in axs2.flat:
     ax.set(xlabel='Time (Days)', ylabel='Price ($)')
@@ -260,8 +258,3 @@ axs2[1].legend(bbox_to_anchor=(0.80, 1.2), fontsize='small', loc=2, borderaxespa
 plt.show()
 
 sns.despine()
-#print(df.head())
-#print(df.axes)
-#print(df.values)
-
-#print(df.shape)
